b2c11b4f1382668ae5f130b73d78e59e28fac951
agerhardssonn <andreasgerhardsson@gmail.com>
2018-05-25 15:04:17 +0200

    Small adjustments

--- tools.py
+++ tools.py
@@ -10,19 +10,18 @@ def fixation_cross(start, duration, delay):
         win.flip()
 
     elif duration < 0:
-        while
-
-    while continueRoutine:
-        fixationCross.draw()
-        win.flip()
-
-        if core.CountdownTimer() < 0:
+        while continueRoutine:
+            fixationCross.draw()
             win.flip()
-            ISI.start(fxc_delay)
-            ISI.complete()
-            continueRoutine = False
 
-            # check for quit (the Esc key)
-        elif event.getKeys(keyList=["escape"]):
-                core.quit()
+            if core.CountdownTimer() < 0:
+                win.flip()
+                ISI.start(fxc_delay)
+                ISI.complete()
+                continueRoutine = False
+
+                # check for quit (the Esc key)
+            elif event.getKeys(keyList=["escape"]):
+                    core.quit()
 # Testar att se om jag ändrar något när
+# Och nu då?
