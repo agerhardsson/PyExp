4b329a8c7030f05b23d7e171e5699c862de8ef57
agerhardssonn <andreasgerhardsson@gmail.com>
2018-12-12 17:24:12 +0100

    Turned into class - Takes define dict of vars, labels and maxinput

--- Misc/gui.py
+++ Misc/gui.py
@@ -11,61 +11,131 @@ from psychopy import event, visual
 To do:
 - create write to file
 - create overwrite protection
-- turn into class
 '''
 
-expInfo = {}
-expInfo['date:'] = time.strftime("%Y-%m-%d")
-expInfo['time:'] = time.strftime("%H:%M")
 
-labels = [('Enter Subject id:', 'subject_id'),
-          ('Enter version number:', 'version'),
-          ('Enter seession:', 'session')]
+class ExperimentInfo():
 
-win = visual.Window()
-stim = {}
-stim['label'] = visual.TextStim(
-    win=win,
-    text='',
-    font=u'Arial')
-stim['input'] = visual.TextStim(
-    win=win,
-    pos=(0.0, -0.2),
-    text='_',
-    font=u'Arial')
+    def __init__(self,
+                 expName='Experiment',
+                 infoDict={'variables':
+                           ['subject_id', 'version', 'session'],
+                           'labels':
+                           ['Enter Subject id:', 'Enter version number:',
+                            'Enter session:'],
+                           'maxInput': [4, 2, 2]},
+                 checkDataFolder='data/',
+                 onlyNumeric=True):
 
-keys = []
-for i in string.ascii_lowercase:
-    keys.append(i)
-for i in string.digits:
-    keys.append(i)
+        self.expInfo = {}
+        self.expInfo['date'] = time.strftime("%Y-%m-%d")
+        self.expInfo['time'] = time.strftime("%H:%M")
+        self.expInfo['exName'] = expName
+        # self.labels = labels
+        self.infoDict = infoDict
+        self.checkDataFolder = checkDataFolder
+        self.onlyNumeric = onlyNumeric
 
+    def checkParticipant(self, subject_id):
+        participants = []
+        for file in os.listdir(self.checkDataFolder):
+            if file.endswith('.txt'):
+                participants.append(file[9:-4])
+            # for i, col_names in enumerate(col_names):
+            #     iapsDict[col_names] = by_cols[i]
 
-for label in labels:
-    input = []
-    while True:
-        stim['label'].setText(label[0])
-        stim['label'].setAutoDraw(True)
-        win.flip()
-        key = event.getKeys(keyList=keys)
-        input.append(key)
-        text = ''.join([y for x in input for y in x])
-        stim['input'].setText(text)
+    def genStim(self):
 
-        stim['input'].setAutoDraw(True)
-        win.flip()
+        stim = {}
+        stim['name'] = visual.TextStim(win=self.win,
+                                       pos=(0.0, 0.5),
+                                       text=self.expInfo['exName'],
+                                       height=0.07,
+                                       font=u'Arial')
+        stim['date'] = visual.TextStim(win=self.win,
+                                       pos=(0.0, 0.4),
+                                       text=self.expInfo['date'],
+                                       height=0.06,
+                                       font=u'Arial')
+        stim['time'] = visual.TextStim(win=self.win,
+                                       pos=(0.0, 0.3),
+                                       text=self.expInfo['time'],
+                                       height=0.06,
+                                       font=u'Arial')
+        stim['label'] = visual.TextStim(win=self.win,
+                                        text='',
+                                        font=u'Arial')
+        stim['input'] = visual.TextStim(win=self.win,
+                                        pos=(0.0, -0.2),
+                                        text='_',
+                                        font=u'Arial')
+        return(stim)
 
-        if event.getKeys(keyList=['backspace']):
+    def genKeys(self):
+        keys = []
+        if not self.onlyNumeric:
+            for i in string.ascii_lowercase:
+                keys.append(i)
+        for i in string.digits:
+            keys.append(i)
+
+        return(keys)
+
+    def expWindow(self):
+        self.win = visual.Window(size=(800, 600),
+                                 fullscr=False,
+                                 screen=0,
+                                 monitor='testMonitor')
+        self.win.mouseVisible = False
+        return self.win
+
+    def run(self):
+        self.win = self.expWindow()
+        stim = self.genStim()
+        keys = self.genKeys()
+
+        # for label in self.labels:
+        for var, lab, max in zip(self.infoDict['variables'],
+                                 self.infoDict['labels'],
+                                 self.infoDict['maxInput']):
             input = []
-            stim['input'].setAutoDraw(False)
-            win.flip()
 
-        if event.getKeys(keyList=['escape', 'return']):
-            break
-    expInfo[label[1]] = text
+            while True:
+                stim['name'].setAutoDraw(True)
+                stim['date'].setAutoDraw(True)
+                stim['time'].setAutoDraw(True)
 
-print(expInfo)
+                stim['label'].setText(lab)
+                stim['label'].setAutoDraw(True)
+                self.win.flip()
+                key = event.getKeys(keyList=keys)
+                input.append(key)
+                text = ''.join([y for x in input for y in x])
+                if len(text) <= max:
+                    stim['input'].setText(text)
+
+                stim['input'].setAutoDraw(True)
+                self.win.flip()
+
+                if event.getKeys(keyList=['backspace']):
+                    input = []
+                    stim['input'].setAutoDraw(False)
+                    self.win.flip()
+
+                if event.getKeys(keyList=['escape', 'return']):
+                    break
+            if not self.onlyNumeric:
+                self.expInfo[var] = text
+            else:
+                self.expInfo[var] = format(int(text),
+                                           '0' + str(max) + 'd')
+
+        return(self.expInfo)
 
 
 # ============================================================================
 # Example
+
+# expInfo = ExperimentInfo()
+# info = expInfo.run()
+# print(info)
